c3e70edb34ce4010d9698b7cfbf4808b37d6fb1c
arquitecturabenito <arquitecturabenito@gmail.com>
2026-02-01 18:49:58 +0100

    feat: Disable unused variable checks in TSConfig
    Temporarily disable `noUnusedLocals` and `noUnusedParameters` in tsconfig.json to allow for ongoing development and refactoring without build failures. These checks will be re-enabled once the code is stable.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { TRANSLATIONS } from './constants';
 import { Language } from './types';
 import WelcomeScreen from './components/WelcomeScreen';
